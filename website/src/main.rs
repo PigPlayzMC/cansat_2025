ed964988f6f8bd8b4a4f756821f3e412f73a0259
PigPlayzMC <charliehbird@gmail.com>
2025-10-22 09:49:48 -0400

    Begin to use tiny_http. Abandoned all home of homemade approach.

--- website/src/main.rs
+++ website/src/main.rs
@@ -21,27 +21,34 @@ fn handle_connections(mut stream: TcpStream) {
 
     if buf_reader.read_line(&mut request_line).is_ok() {
         let request_line: &str = request_line.trim();
+        println!("NEW request: {}", request_line);
 
         if request_line.starts_with("GET") {
             let parts: Vec<&str> = request_line.split_whitespace().collect();
             handle_get_requests(&mut stream, parts[1]);
         } else if request_line.starts_with("POST") {
             handle_post_request(&mut stream);
-        } else {
-            println!("Unsupported HTTP method recieved.");
+        } else { // Deny all non GET or POST requests, such as trace and put. We only do the bare minimum here!
+            println!("ERROR 405: HTTP method {} received. This is not supported.", request_line);
+            let response: String = format!(
+                "HTTP/1.1 405 Method Not Allowed\r\nContent-Length: 0\r\nAllow: {}\r\n\r\n",
+                "GET, POST"
+            );
+        
+            stream.write_all(response.as_bytes()).unwrap();
         }
     }
 }
 
 fn handle_get_requests(stream: &mut TcpStream, request_path: &str) {
-    let file_path: String = if request_path == "/" {
+    let file_path: String = if request_path == "/" { // Catch wrong requests and redirect rather than serving an error
         "home.html".to_string()
     } else {
         request_path.trim_start_matches('/').to_string()
     };
 
-    println!("File path = {}", file_path);
-    let mut mut_file_path: String = file_path.clone();
+    ////println!("File path = {}", file_path);
+    let mut_file_path: String = file_path.clone();
     let file: String = mut_file_path.replace("website/", "");
     let content: String = fs::read_to_string(file).unwrap_or_else(|_| {
         String::from("<h1>404 Not Found</h1>")
@@ -58,12 +65,14 @@ fn handle_get_requests(stream: &mut TcpStream, request_path: &str) {
         "image/svg+xml"
     } else if file_path.ends_with(".jpeg") || file_path.ends_with(".jpg") {
         "image/jpeg" // Do not use jpg, this is a safety catch just in case
+    } else if file_path.ends_with(".ico") {
+        "image/ico" //TODO Test
     } else if file_path.ends_with(".json") {
         "application/json"
     } else {
         "text/plain"
-    }; //TODO JSON, IMAGES
-    println!("Content Type = {}", content_type);
+    };
+    ////println!("Content Type = {}", content_type);
     
     let response: String = format!(
         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\n\r\n{}",
@@ -76,24 +85,5 @@ fn handle_get_requests(stream: &mut TcpStream, request_path: &str) {
 }
 
 fn handle_post_request(stream: &mut TcpStream) {
-    let mut buf_reader: BufReader<&mut TcpStream> = BufReader::new(stream);
-    let mut headers: String = String::new();
-    let mut body: String = String::new();
-
-    // Read headers until empty line with line break char
-    while let Ok(bytes_read) = buf_reader.read_line(&mut headers) {
-        if bytes_read == 0 || headers == "\r\n" {
-            break;
-        }
-        headers.clear();
-    }
-
-    buf_reader.read_to_string(&mut body).unwrap();
     
-    //TODO determine desired action
-    
-    //TODO rest of process
-
-    let response: &'static str = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello World!";
-    stream.write_all(response.as_bytes()).unwrap();
 }
