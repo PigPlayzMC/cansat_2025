f25fe0523a855c78c16e86c9c1f15159185ca23d
PigPlayzMC <charliehbird@gmail.com>
2026-02-11 12:37:06 +0000

    Satellite micropython code

--- satellite/bmp280.py
+++ satellite/bmp280.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import board # type: ignore
 import busio # type: ignore
 import adafruit_bmp280 # type: ignore
@@ -11,3 +12,23 @@ def read_temperature():
 
 def read_pressure():
     return bmp280_sensor.pressure
+=======
+import board as b
+import busio as bus
+import adafruit_bmp280 as ada_bmp280
+
+i2c = bus.I2C(b.GP15, b.GP14)
+bmp280 = ada_bmp280.Adafruit_BMP280_I2C(i2c)
+
+def read_temperature():
+    return bmp280.temperature
+
+def read_pressure():
+    return bmp280.pressure
+
+def read_temp_pressure_array():
+    return [bmp280.temperature, bmp280.pressure]
+
+def display_environment():
+    print(f"Temperature: {bmp280.temperature}˚c\nPressure: {bmp280.pressure}")
+>>>>>>> Stashed changes
