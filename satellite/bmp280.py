0f845049bf7db444947e349cbc972909fbbb16b4
PigPlayzMC <charliehbird@gmail.com>
2026-02-11 12:38:54 +0000

    Fix commit conflict

--- satellite/bmp280.py
+++ satellite/bmp280.py
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-import board # type: ignore
-import busio # type: ignore
-import adafruit_bmp280 # type: ignore
-
-i2c = busio.I2c(scl = board.GP15, sda = board.GP14)
-
-bmp280_sensor = adafruit_bmp280.Adafruit_BMP280_I2c(i2c)
-
-def read_temperature():
-    return bmp280_sensor.temperature
-
-def read_pressure():
-    return bmp280_sensor.pressure
-=======
 import board as b
 import busio as bus
 import adafruit_bmp280 as ada_bmp280
@@ -31,4 +16,3 @@ def read_temp_pressure_array():
 
 def display_environment():
     print(f"Temperature: {bmp280.temperature}˚c\nPressure: {bmp280.pressure}")
->>>>>>> Stashed changes
