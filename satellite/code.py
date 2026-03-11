be94ed354f96fb3e0dbc9390258cfe4f72f113c0
PigPlayzMC <charliehbird@gmail.com>
2026-03-11 13:33:08 +0000

    Adjust time calculation

--- satellite/code.py
+++ satellite/code.py
@@ -1,6 +1,8 @@
 import digitalio as dg
 import board as b
 import time as t
+import adafruit_ticks as tk
+import math as m
 
 # Internal imports
 import bmp280
@@ -16,8 +18,14 @@ SEA_LEVEL_PRESSURE = 101325
 led = dg.DigitalInOut(b.GP25)
 led.direction = dg.Direction.OUTPUT
 
+last_data = []
+
 print("CanSat ready!")
 radio.send("CanSat ready!")
+
+def calculate_altitude(pressure):
+    # Barometric formula (troposphere)
+    return (1 - (pressure / SEA_LEVEL_PRESSURE) ** (1 / 5.25588)) / 2.25577e-5
 
 while true:
     led.value = false
@@ -25,15 +33,18 @@ while true:
     led.value = true
     t.sleep(0.5)
 
-    current_time = t.ticks_ms()
+    current_time = tk.ticks_ms() / 1000 - 536788
 
     temperature = bmp280.temperature()
     pressure = bmp280.pressure()
     altitude = calculate_altitude(pressure * 100) # BMP280 outputs in hPa
 
-    if last_data is not None:
+    if last_data != []:
         delta_alt = altitude - last_data[2]
-        delta_time = time.ticks_diff(current_time, last_data[3] / 1000) # Should be 1 in an ideal world
+        
+        print(f"{current_time}, {last_data[4]}")
+        
+        delta_time = tk.ticks_diff(m.floor(current_time), m.floor(last_data[4])) # Should be 1 in an ideal world
 
         velocity = delta_alt / delta_time
     else:
@@ -45,8 +56,5 @@ while true:
     radio.send(f"{current_time}, {velocity}, {temperature}, {altitude}, {pressure}")
 
     last_data = [temperature, pressure, altitude, current_time, velocity]
-
-
-def calculate_altitude(pressure):
-    # Barometric formula (troposphere)
-    return (1 - (pressure / SEA_LEVEL_PRESSURE) ** (1 / 5.25588)) / 2.25577e-5
+    
+    print("Updated last_data[]")
