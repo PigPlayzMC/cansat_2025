bbb54f615428eaee59f92847e99bbdf1d7988599
PigPlayzMC <charliehbird@gmail.com>
2026-03-23 19:37:46 +0000

    retrieval code

--- laptop_basestation/retrieve_data.py
+++ laptop_basestation/retrieve_data.py
@@ -1,1 +1,16 @@
-# Panic stations coding for the win
+# Panic stations coding for the win!
+
+import serial
+
+port = "/dev/cu.usbmodem101" # SECOND USB-C
+baudrate = 115200
+serial_connection = serial.Serial(port, baudrate)
+
+# Env files are for cowards and people with something to hide
+values_file = open("/Users/cb/Documents/GitHub/cansat_2025/laptop_basestation/assets/values.csv")
+
+while True:
+    data = serial_connection.read(128)
+    if data = b"EOF":
+        break
+    values_file.write(data)
